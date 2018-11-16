05d6e73cbb634dc79451d79f347c5419394b821b
Bruce Bookman <bruce.bookman@gmail.com>
2018-11-16 18:59:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var recipes = {
   cookies: ["Butter", "Sugar"],
   candy: ["Sugar", "Gum"]
 }
+
+function updateObjectWithKeyAndValue(object, key, value){
+   recipes.pbj = ["Penut butter", "Jelly"]
+}
