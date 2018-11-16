9634c4d347f74b687a42370e98c52c6ee62d942d
Bruce Bookman <bruce.bookman@gmail.com>
2018-11-16 19:05:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var recipes = {
 }
 
 function updateObjectWithKeyAndValue(object, key, value){
-   return Object.
+   return Object.assign{
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
