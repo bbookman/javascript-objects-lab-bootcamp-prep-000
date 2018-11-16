525cc1b4bc63736b7185c54115675dc637e00991
Bruce Bookman <bruce.bookman@gmail.com>
2018-11-16 19:15:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
-  return object
+  object[key] = value;
+  return object;
 }
 
 
 function deleteFromObjectByKey(object, key){
-  var newObj = Object.assign({}, object)
-  delete newObj[key]
-  return  newObj
+  var newObj = Object.assign({}, object);
+  delete newObj[key];
+  return  newObj;
 }
