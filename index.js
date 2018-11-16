adca75eae37d85e6629e6453e289df4f61bbcf09
Bruce Bookman <bruce.bookman@gmail.com>
2018-11-16 19:10:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,10 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
 }
+
+
+function deleteFromObjectByKey(object, key)
+  var new_object = object
+  delete new_object[key]
+  return new_object
+}
